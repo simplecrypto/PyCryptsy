aa9277028d5519bcb6e8a91821cdb4e1195e1dc8
Joshua Sumali <j.sumali@gmail.com>
2013-07-19 23:19:05 -0400

    add example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 # PyCryptsy Example
 #
-# Copyright Â© 2013 Scott Alfter
+# Copyright © 2013 Scott Alfter
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -22,27 +22,64 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+import time as t
+import logging
+
 from PyCryptsy import PyCryptsy
 
-api=PyCryptsy('public_key_goes_here', 'private_key_goes_here')
+public_key = 'TODO'
+private_key = 'TODO'
+
+api = PyCryptsy(public_key, private_key)
+
+# Don't trade these currencies
+non_traded_currencies = ('BTC', 'LTC')
 
 # currencies to trade
 
-src="YAC"
-dest="BTC"
+#src_currencies = ['NVC', 'MNC', 'WDC', 'DGC', 'LKY', 'ARG', 'PXC', 'NRB']
+dest_currency = "BTC"
 
 # trade multiplier (set to 1 to trade at lowest buy price)
+multiplier = 1
+percent_to_sell = 1.0
 
-multiplier=1.01
 
-# get target price
+def setup_logging():
+    global console
+    logging.basicConfig(filename='trades.log.txt', level=logging.INFO,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    logging.getLogger('').addHandler(console)
 
-target=api.GetBuyPrice(src, dest)*multiplier
+def get_non_zero_currencies():
+    balances = api.GetAvailableBalances()
+    balances_available = balances["return"]["balances_available"] #CUR -> amt
+    non_zero_currencies = {cur: float(coin_amt) for (cur, coin_amt) in balances_available.iteritems() if
+                           float(coin_amt) > 0 and cur not in non_traded_currencies}
+    # print non_zero_currencies
+    return non_zero_currencies
 
-# get available balance
+# src_currencies dict of CUR -> amt available
+def do_trades(src_currencies):
+    for (currency, coins_available) in src_currencies:
+        # get target price
+        target = api.GetBuyPrice(currency, dest_currency) * multiplier
 
-avail=api.GetAvailableBalance(src)
+        logging.info("Target: %s, Avail: %s" % (target, coins_available))
+        if coins_available > 0:
+            units_to_sell = coins_available * percent_to_sell
+            logging.info(
+                "Creating sell order from %s to %s, units: %d, target price: %d" % (currency, dest_currency, units_to_sell, target))
+            result = api.CreateSellOrder(currency, dest_currency, units_to_sell, target)
+            # {u'orderid': u'532446', u'moreinfo': u'Your Sell order has been placed for<br><b>353.81173381 NRB @ 0.00016990 BTC</b> each.<br>Order ID: <b>532446</b>', u'success': u'1'}
+            logging.info("Result: OrderId: %s, Info: %s, Success: %s" % (result['orderid'], result['moreinfo'], result['success']))
 
-# trade 10% of available balance
-
-print api.CreateSellOrder(src, dest, avail/10, target)
+# start trading
+setup_logging()
+while True:
+    non_zero_currencies = get_non_zero_currencies()
+    do_trades(non_zero_currencies)
+    logging.info("Sleeping for 600 seconds ...")
+    t.sleep(600) # Sleep for 10 minutes
